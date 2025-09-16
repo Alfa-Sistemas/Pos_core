c7e1fb5b4a5fe94f7a30e3e76b1f81498146a0fc
Nickollas <nickjohalb@outlook.com>
2025-09-16 11:53:05 -0300

    correções removendo package nativo

--- lib/pos_core.dart
+++ lib/pos_core.dart
@@ -1,3 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get_it/get_it.dart';
+import 'package:pos_core/domain/usecases/usecase_interface.dart';
+
+var printUsecase = GetIt.I.get<UsecaseInterface>(instanceName: "print");
+
 class PosCore {
-  
+  Future<String> imprimirArquivo(BuildContext context, String filePath) async {
+    return await printUsecase.execute(filePath);
+  }
 }
